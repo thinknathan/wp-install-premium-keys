7af16dfab1e36449fe6ce5cadbddb2b86cdf0666
Nathan Bolton <nathan@nathanbolton.ca>
2017-08-22 11:17:36 -0700

    Fixed ACF install

--- wp-install-premium-keys.php
+++ wp-install-premium-keys.php
@@ -3,7 +3,7 @@
 Plugin Name:  Premium Key Installer
 Plugin URI:   https://github.com/thinknathan/
 Description:  Sets keys for premium WP plugins.
-Version:      1.0.1
+Version:      1.0.2
 Author:       Think_Nathan
 Author URI:   https://thinknathan.ca/
 License:      MIT License
@@ -21,7 +21,13 @@ function custom_install_premium_keys() {
   }
   // Advanced Custom Fields Pro
   if ( class_exists('acf') && getenv('ACF_PRO_KEY') ) {
-    update_option( 'acf_pro_license', getenv('ACF_PRO_KEY') );
+    $save = array(
+      'key'	=> getenv('ACF_PRO_KEY'),
+      'url'	=> home_url()
+	);
+    $save = maybe_serialize($save);
+    $save = base64_encode($save);
+    update_option( 'acf_pro_license', $save );
   }
 }
 add_action( 'plugins_loaded', 'custom_install_premium_keys' );
