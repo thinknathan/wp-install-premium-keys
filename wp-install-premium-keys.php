0cf5fd03a6c544d142393567c1a4ee0568ccc5ff
Nathan Bolton <nathan@nathanbolton.ca>
2018-01-24 22:00:21 -0800

    Major update

--- wp-install-premium-keys.php
+++ wp-install-premium-keys.php
@@ -3,7 +3,7 @@
 Plugin Name:  Premium Key Installer
 Plugin URI:   https://github.com/thinknathan/
 Description:  Sets keys for premium WP plugins.
-Version:      1.0.2
+Version:      1.0.3
 Author:       Think_Nathan
 Author URI:   https://thinknathan.ca/
 License:      MIT License
@@ -14,20 +14,48 @@ if ( getenv('GFORMS_KEY') ) {
   define( 'GF_LICENSE_KEY', getenv('GFORMS_KEY') );
 }
 
+// WP Rocket
+if ( getenv('WP_ROCKET_KEY') ) {
+	define( 'WP_ROCKET_KEY', getenv('WP_ROCKET_KEY') );
+}
+
+if ( getenv('WP_ROCKET_EMAIL') ) {
+	define( 'WP_ROCKET_EMAIL', getenv('WP_ROCKET_EMAIL') );
+}
+
+// ACF Solution - Austin Ginder - https://anchor.host/2017/05/16/preloading-advanced-custom-fields-pro-license-key/
+
 function custom_install_premium_keys() {
-  // WP Rocket
-  if ( function_exists ('update_rocket_option') && getenv('WP_ROCKET_KEY') ) {
-    update_rocket_option( 'consumer_key', getenv('WP_ROCKET_KEY') );
-  }
-  // Advanced Custom Fields Pro
-  if ( class_exists('acf') && getenv('ACF_PRO_KEY') ) {
-    $save = array(
-      'key'	=> getenv('ACF_PRO_KEY'),
-      'url'	=> home_url()
-	);
-    $save = maybe_serialize($save);
-    $save = base64_encode($save);
-    update_option( 'acf_pro_license', $save );
+  if ( is_plugin_active( 'advanced-custom-fields-pro/acf.php' ) ) {
+
+    // Loads ACF plugin
+    include_once( ABSPATH . '../app/plugins/advanced-custom-fields-pro/acf.php');
+
+    // connect
+    $post = array(
+      'acf_license'	=> getenv('ACF_PRO_KEY'),
+      'acf_version'	=> acf_get_setting('version'),
+      'wp_name'			=> get_bloginfo('name'),
+      'wp_url'			=> home_url(),
+      'wp_version'	=> get_bloginfo('version'),
+      'wp_language'	=> get_bloginfo('language'),
+      'wp_timezone'	=> get_option('timezone_string'),
+    );
+
+    // connect
+    $response = acf_updates()->request('v2/plugins/activate?p=pro', $post);
+
+    // ensure response is expected JSON array (not string)
+    if( is_string($response) ) {
+      $response = new WP_Error( 'server_error', esc_html($response) );
+    }
+
+    // success
+    if( $response['status'] == 1 ) {
+      acf_pro_update_license( $response['license'] ); // update license
+    }
+    echo $response['message']; // show message
+
   }
 }
-add_action( 'plugins_loaded', 'custom_install_premium_keys' );
+add_action( 'switch_theme', 'custom_install_premium_keys' );
